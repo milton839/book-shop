baf6b241a9661d616cda1a2079675399511c747d
milton839 <miltonbiswas839@gmail.com>
2024-04-02 00:09:13 +0600

    booklist done

--- src/components/Home/BookMarket/BooksMarket.jsx
+++ src/components/Home/BookMarket/BooksMarket.jsx
@@ -1,12 +1,20 @@
+import { useEffect, useState } from "react";
 import BooksList from "./BooksList";
 import SearchBook from "./SearchBook";
 
 
 const BooksMarket = () => {
+    const [books, setBooks] = useState([]);
+
+    useEffect(()=>{
+        fetch("/public/data.JSON")
+        .then(res=>res.json())
+        .then(data=>setBooks(data))
+    }, [])
   return (
     <main className="my-10 lg:my-14">
       <SearchBook />
-      <BooksList />
+      <BooksList books = {books} />
     </main>
   )
 }
